6c5e4ecdec8bacf1cc50974fdf1725b323f6f3a5
GianiStatie <gianistatie@gmail.com>
2025-06-12 10:01:26 +0300

    fix if-else agent

--- scripts/01_if_else_agent.py
+++ scripts/01_if_else_agent.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 
 import numpy as np
@@ -9,44 +10,54 @@ parser.add_argument(
     "--env_path",
     default=None,
     type=str,
-    help="The Godot binary to use, do not include for in editor training",
+    help="The path to the Godot game directory (get this by typing 'path' in the game's developer console)",
 )
 parser.add_argument(
-    "--viz",
-    action="store_true",
-    help="If set, the simulation will be displayed in a window during training. Otherwise "
-    "training will run without rendering the simulation. This setting does not apply to in-editor training.",
-    default=True,
+    "--env_name",
+    default="DragonJump",
+    type=str,
+    help="The Godot binary to use, do not include for in editor training",
 )
 parser.add_argument("--seed", type=int, default=42, help="seed of the experiment")
 parser.add_argument(
-    "--n_parallel",
-    default=1,
+    "--nb_agents",
+    default=10,
     type=int,
-    help="How many instances of the environment executable to "
-    "launch - requires --env_path to be set if > 1.",
+    help="How many agents to launch in the environment. "
+    "Requires --nb_agents to be set if > 1.",
 )
 parser.add_argument(
     "--speedup", default=1, type=int, help="Whether to speed up the physics in the env"
 )
 
 
-def if_else_agent(ovservation):
-    # THE BIGGER THE VALUE, THE CLOSER YOU ARE TO A WALL
-    if ovservation["sensors"][8] + ovservation["sensors"][9] > 1.2:
-        return 1
-    return 0
+def if_else_agent(sensors, step_number):
+    action = 0
+    
+    # JUMP FOR FIRST 3 STEPS TO START THE GAME
+    if step_number < 3:
+        action = step_number % 2
+
+    # JUMP IF YOU ARE CLOSER THAN 1.2 UNITS TO A WALL
+    elif sensors[8] + sensors[9] > 1:
+        action = 1
+    return np.array([action])
 
 
 def main():
     args, extras = parser.parse_known_args()
 
+    env_path = None
+    if args.env_path:
+        env_path = os.path.join(args.env_path, args.env_name)
+
     env = StableBaselinesGodotEnv(
-        env_path=args.env_path,
-        show_window=args.viz,
+        env_path=env_path,
+        show_window=True, # can't get 2d observation without rendering
         seed=args.seed,
-        n_parallel=args.n_parallel,
         speedup=args.speedup,
+        nb_agents=args.nb_agents,
+        host_binding=True
     )
 
     # GET THE INITIAL STATE OF THE GAME
@@ -54,14 +65,12 @@ def main():
 
     # GET NUMBER OF CONCURENT AGENTS IN ONE ENVIRONMENT
     nb_agents = len(obs["obs"])
-
+    
+    step_number = 0
     while True:
-        # CALCULATE BEST ACTION TO TAKE
-        action = if_else_agent(obs["obs"][0])
-
-        # TELL ALL AGENTS TO TAKE THE SAME ACTION
-        actions = [[action] for _ in range(nb_agents)]
-
+        # TAKE AN ACTION FOR EACH AGENT
+        actions = [if_else_agent(obs["obs"][0], step_number) for _ in range(nb_agents)]
+        
         # FORMAT THE ACTIONS AS A NUMPY ARRAY
         actions = np.array(actions, dtype=np.int64)
 
@@ -71,6 +80,8 @@ def main():
         # IF ANY OF THE AGENTS FINISHES OR TIME EXPIRES END THE LOOP
         if any(done):
             break
+
+        step_number += 1
 
     env.close()
 
