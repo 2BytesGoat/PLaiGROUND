025d082565374d33260e8a2c22e38dcfd8f74c46
GianiStatie <gianistatie@gmail.com>
2026-05-01 22:14:42 +0300

    add lin reg

--- src/processing/feature_engineering.py
+++ src/processing/feature_engineering.py
@@ -1,5 +1,16 @@
 import numpy as np
 
+DIRECTIONS = [
+        (-1, 0),  # up
+        (-1, 1),  # up-right
+        (0, 1),   # right
+        (1, 1),   # down-right
+        (1, 0),   # down
+        (1, -1),  # down-left
+        (0, -1),  # left
+        (-1, -1), # up-left
+    ]
+
 
 def compute_can_jump(sensor_values: dict, session_info: dict) -> float:
     """
@@ -20,16 +31,6 @@ def compute_object_sensors(
     player_cell: tuple[int, int] = (3, 3),
     max_steps: int = 4,
 ) -> list[float]:
-    directions = [
-        (0, 1),   # right
-        (0, -1),  # left
-        (-1, 0),  # up
-        (1, 0),   # down
-        (-1, -1), # up-left
-        (-1, 1),  # up-right
-        (1, -1),  # down-left
-        (1, 1),   # down-right
-    ]
     return [
         _distance_to_object_in_direction(
             grid=grid,
@@ -38,8 +39,20 @@ def compute_object_sensors(
             direction=direction,
             max_steps=max_steps,
         )
-        for direction in directions
+        for direction in DIRECTIONS
     ]
+
+
+def compute_raycast_sensors(
+    grid: np.ndarray,
+    player_cell: tuple[int, int] = (3, 3),
+    max_steps: int = 4,
+) -> list[float]:
+    raycasts, object_types = zip(*[
+        _raycast_in_direction(grid, player_cell, direction, max_steps)
+        for direction in DIRECTIONS
+    ])
+    return list(raycasts), list(object_types)
 
 
 def _distance_to_object_in_direction(
@@ -56,7 +69,26 @@ def _distance_to_object_in_direction(
         next_row = row + d_row * step
         next_col = col + d_col * step
         if not (0 <= next_row < grid.shape[0] and 0 <= next_col < grid.shape[1]):
-            break
+            break # out of bounds
         if grid[next_row, next_col] == object_index:
-            return step * 0.25
-    return 1.0
+            return step * 0.25 # found object
+    return 1.0 # no object found
+
+
+def _raycast_in_direction(
+    grid: np.ndarray,
+    player_cell: tuple[int, int],
+    direction: tuple[int, int],
+    max_steps: int = 4,
+) -> tuple[float, int]:
+    row, col = player_cell
+    d_row, d_col = direction
+
+    for step in range(0, max_steps):
+        next_row = row + d_row * step
+        next_col = col + d_col * step
+        if not (0 <= next_row < grid.shape[0] and 0 <= next_col < grid.shape[1]):
+            break # out of bounds
+        if grid[next_row, next_col] != 0:
+            return step * 0.25, grid[next_row, next_col] # found object
+    return 1.0, 0 # no object found
